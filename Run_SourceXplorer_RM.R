fa30664391579346299acc9b40f441ddaf0afb85
RhyMcMillan <rhy.mcmillan@gmail.com>
2022-04-28 14:16:06 -0700

    Added initialization script.

---
+++ Run_SourceXplorer_RM.R
@@ -0,0 +1,9 @@
+
+#Install the SourceXplorer Package from https://github.com/RhyMcMillan/SourceXplorer
+devtools::install_github("RhyMcMillan/SourceXplorer")
+
+#Run the SourceXplorer App
+SourceXplorer::SourceXplorer()
+
+#Run the quickpRep App
+SourceXplorer::quickpRep()
